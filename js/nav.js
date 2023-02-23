a4edb1ad46928f68ccdf10813df25f580cf404b6
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-02-23 00:06:11 -0300

    nav

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+const navMenu = document.getElementById("navMenu");
+const closeMenu = document.getElementById("closeMenu");
+const toggleMenu = document.getElementById("toggleMenu");
+
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.add("nav_show");
+});
+
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("nav_show");
+});
