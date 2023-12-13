dfe6ae3e695ea23bc604adb2c799b6eed1325e61
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-12-13 15:01:09 -0300

    Nodemailer

--- js/mail.js
+++ js/mail.js
@@ -1,6 +1,6 @@
 let productionMailRoute = "https://mail-antiguohobby.onrender.com/mail";
 let devMailRoute = "http://localhost:8080/mail";
-let enviroment = "dev"; // prod || dev
+let enviroment = "prod"; // prod || dev
 let mailRoute = "";
 
 switch (enviroment) {
@@ -12,43 +12,57 @@ switch (enviroment) {
     break;
 }
 
-window.addEventListener('load',() => {
-    const buttonSend = document.querySelector("#buttonSend")
-    const contactForm = document.querySelector("#contactForm")
-    buttonSend.addEventListener("click", (event) =>{
-      e.preventDefault();
-      grecaptcha.ready(function() {
-        grecaptcha.execute('6LdhOzApAAAAAMUpwQYhi95TGeEqFgXH80_jAeNh', {action: 'submit'}).then(function(token) {
-            // Add your logic to submit to your backend server here.
-            postData()
-        });
-      });
-    });
-})
+window.addEventListener("load", () => {
+  const buttonSend = document.querySelector("#buttonSend");
+  const contactForm = document.querySelector("#contactForm");
 
-const getData = () => {
-    const datos = new FormData(contactForm)
-    const datosProcesados = Object.fromEntries(datos.entries())
-    contactForm.reset()
-    return datosProcesados
+  contactForm.addEventListener("submit", runVerify);
+});
+
+const runVerify = (e) => {
+  e.preventDefault()
+  runCaptcha()
 }
 
-const postData = async () => {
-  console.log("holii");
-  const newMessage = getData();
-  console.log(newMessage);
+const runCaptcha = () => {
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LdhOzApAAAAAMUpwQYhi95TGeEqFgXH80_jAeNh', {action: 'submit'}).then(function(token) {
+      // Add your logic to submit to your backend server here.
+      const captcha = token
+      postData(captcha)
+    });
+  });
+}
+
+const getData = (captcha) => {
+  let datos = new FormData(contactForm);
+  let datosProcesados = Object.fromEntries(datos.entries());
+  datosProcesados.captcha = captcha
+  return datosProcesados;
+};
+
+const postData = async (captcha) => {
+  const newMessage = getData(captcha);
   try {
     const response = await fetch(mailRoute, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { 
+        "Accept": "application/json, text-/plain, */*",
+        "Content-Type": "application/json" 
+      },
       body: JSON.stringify(newMessage),
-    });
-    if (response.ok) {
-    //    const jsonResponse = await json.response() 
-      alert("Gracias por contactarnos.");
-    }
+    }).then((res) => res.json()).then((data)=>{
+      Swal.fire({
+        text: `Mensaje: ${data.mensaje}`,
+        icon: "success"
+      });
+      contactForm.reset()
+    })  
+
   } catch (error) {
-    console.log(error);
+    Swal.fire({
+      text: `Mensaje: ${data.mensaje}`,
+      icon: "error"
+    });
   }
 };
-
